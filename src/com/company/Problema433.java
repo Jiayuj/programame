3de707bdfc2d95446ade4cf822ed939418bff781
Jiayu <jiayuji@elpuig.xeill.net>
2018-10-30 16:43:51 +0100

    sdfsfd

--- src/com/company/Problema433.java
+++ src/com/company/Problema433.java
@@ -1,4 +1,20 @@
 package com.company;
-
+import java.util.Scanner;
 public class Problema433 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int uvas = 1;
+        while (uvas > 0) {
+            int c = 0, b = 0;
+            uvas = in.nextInt();
+            while (uvas > b) {
+                c++;
+                b = c + b;
+            }
+            if (c != 0) {
+                System.out.println(c);
+            }
+        }
+    }
 }
+
