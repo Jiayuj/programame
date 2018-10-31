904382d7d535921d628ff9f16acd4d1c2ece4fda
Jiayu <jiayuji@elpuig.xeill.net>
2018-10-31 20:28:10 +0100

    sdfsfd

---
+++ src/com/company/Problema127.java
@@ -0,0 +1,43 @@
+package com.company;
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Problema127 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int v = in.nextInt();
+        in.nextLine();
+        while (v != 0) {
+            ArrayList<String> fe=new ArrayList<String>();
+            String nombre = in.nextLine();
+            String n[] = nombre.split(" ");
+            for (int i = 0; i <n.length ; i++) {
+                if (n[i].equals("F")){
+                    break;
+                }else fe.add(n[i]);
+            }
+            int s = Integer.parseInt(n[n.length-2]);
+            int c = Integer.parseInt(n[n.length-1]);
+
+            if (fe.size()<s){
+                System.out.println("TODOS TIENEN CAMA");
+            }else if (s==0){
+                System.out.println("NADIE TIENE CAMA");
+            }else {
+                int d=c-1;
+                while (fe.size()!=s){
+                    if (d<fe.size()){
+                    fe.remove(d);
+                    d=d+c-1;
+                    }else if (d>fe.size()){
+                        d=d-fe.size();
+                    }
+                }
+                for (int i = 0; i <fe.size() ; i++) {
+                    System.out.println(fe[i]+" ");
+                }
+            }
+            v--;
+        }
+    }
+}
+
