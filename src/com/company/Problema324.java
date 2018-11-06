f1af05b316fe81f6e5db1df6036543d905058e48
Jiayuj <44613675+Jiayuj@users.noreply.github.com>
2018-11-06 23:25:53 +0100

    q

--- src/com/company/Problema324.java
+++ src/com/company/Problema324.java
@@ -1,31 +1,42 @@
 package com.company;
-
 import java.util.Scanner;
-
 public class Problema324 {
     public static void main (String[] args){
+        int c=0,l=0;
         Scanner sc = new Scanner(System.in);
-        int q = sc.nextInt(),c=0;
+        l = sc.nextInt();
         sc.nextLine();
-            String w = sc.nextLine();
-            String e[] = w.split(" ");
-            int n[] = new int[e.length];
-            int n1[] = new int[e.length];
-            for (int i = 0; i <e.length ; i++) {
-                n[i]=Integer.parseInt(e[i]);
-                n1[i]=Integer.parseInt(e[i]);
-            }
-            while (q != 0){
+        String w = sc.nextLine();
+        String e[] = w.split(" ");
+        int n[] = new int[e.length];
+        int n1[] = new int[e.length];
+        int n2[] = new int[e.length];
+        for (int i = 0; i <e.length ; i++) {
+            n[i]=Integer.parseInt(e[i]);
+        }
+        for (int i = 0; i < e.length; i++) {
+            int g = n[i];
+            n1[n[i] - 1] = g;
+            c++;
+        }
+
+        if (n1 == n) {
+            System.out.println(c/3);
+        }else{
+            while (n1 != n) {
                 for (int i = 0; i < e.length; i++) {
                     int g = n1[i];
-                    n[g - 1] = g;
+                    n2[n[i] - 1] = g;
                     c++;
                 }
                 for (int i = 0; i < e.length; i++) {
-                    int g = n[i];
-                    n[g - 1] = g;
+                    int g = n2[i];
+                    n1[n[i] - 1] = g;
+                    c++;
                 }
-            q--;
+            }
+            System.out.println(c/3);
         }
     }
 }
+
