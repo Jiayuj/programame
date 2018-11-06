b873d1621bbc4eda8005692967ca49140fd5ce9c
Jiayu <jiayuji@elpuig.xeill.net>
2018-11-06 18:23:17 +0100

    sdfsfd

--- src/com/company/Problema127.java
+++ src/com/company/Problema127.java
@@ -33,7 +33,9 @@ public class Problema127 {
                     }
                 }
                 for (int i = 0; i <fe.size() ; i++) {
-                    System.out.print(fe.get(i)+" ");
+                    if (i==0) {
+                        System.out.print(fe.get(i));
+                    }else System.out.print(" "+fe.get(i));
                 }
             }
             v--;
---
+++ src/com/company/Problema324.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Problema324 {
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        int q = sc.nextInt(),c=0;
+        sc.nextLine();
+
+            String w = sc.nextLine();
+            String e[] = w.split(" ");
+            int n[] = new int[e.length];
+            int n1[] = new int[e.length];
+            for (int i = 0; i <e.length ; i++) {
+                n[i]=Integer.parseInt(e[i]);
+                n1[i]=Integer.parseInt(e[i]);
+            }
+            while (q != 0){
+                for (int i = 0; i < e.length; i++) {
+                    int g = n1[i];
+                    n[g - 1] = g;
+                    c++;
+                }
+                for (int i = 0; i < e.length; i++) {
+                    int g = n[i];
+                    n[g - 1] = g;
+                    c++;
+                }
+            q--;
+        }
+    }
+}
