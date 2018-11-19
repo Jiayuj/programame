55fa5ea085c7c77ba14a99d9ada7618e0eddb1ee
Jiayu <jiayuji@elpuig.xeill.net>
2018-11-19 17:36:49 +0100

    sdfsfd

---
+++ src/com/company/E1.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class E1 {
+    public static void main (String[] args){
+        Scanner sr = new Scanner(System.in);
+        int q=0;
+        int n = sr.nextInt();
+        for (int i = 0; i < n; i++) {
+            int r=n%i;
+            if (r=0){
+                q++;
+            }
+        }
+        System.out.println(q);
+    }
+}
--- src/com/company/TipoExamen.java
+++ src/com/company/TipoExamen.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class TipoExamen {
     public static void main (String[] args){
         Scanner sr = new Scanner(System.in);
-        int[] n = sr.nextInt();
+        String n = sr.nextInt();
 
     }
 }
